5c5c30c657d52affda93ec1a0a2d80432f7fb4fb
Roland <rolandoliveira17@hotmail.com>
2022-12-29 00:41:37 +0000

    change background

--- script.js
+++ script.js
@@ -7,14 +7,4 @@ function toggleMode() {
   //} else {
   // html.classList.add("light")
   // }
-
-  // pegar a tag image
-  const image = document.querySelector("#profile img")
-  //substituir a imagem
-  if (html.classList.contains("light")) {
-    // se tiver light mode adcionar a imagem light
-    image.setAttribute("scr", "assets/avatar-light.png")
-  } else {
-    image.setAttribute("scr", "assets/Avatar.png")
-  }
 }
